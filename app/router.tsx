f068a931895f65dbab3d39325d341db38bad6756
Tom Ballinger <tom@convex.dev>
2024-07-24 13:11:23 -0700

    no longer typechecks

--- app/router.tsx
+++ app/router.tsx
@@ -1,12 +1,16 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
 
 export function createRouter() {
+  const queryClient = new QueryClient();
   const router = createTanStackRouter({
     routeTree,
   });
+  const augmented = routerWithQueryClient(router, queryClient);
 
-  return router;
+  return augmented;
 }
 
 declare module "@tanstack/react-router" {
